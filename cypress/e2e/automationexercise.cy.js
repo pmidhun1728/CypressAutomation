95ed26cdc0c7189d0dc67eb293fe45bf6adbe68b
Midhun <42476012+pmidhun1728@users.noreply.github.com>
2023-06-13 15:05:14 -0400

    Test case for Adding Dresses to cart

--- cypress/e2e/automationexercise.cy.js
+++ cypress/e2e/automationexercise.cy.js
@@ -4,15 +4,32 @@ describe('Automation Exercise', () => {
   beforeEach(() => {
     // Code to execute before each test case goes here
     cy.visit('https://automationexercise.com/').gettitle;
-    cy.xpath('//*[text()=" Signup / Login"]').click();
-    cy.xpath('//*[@id="form"]/div/div/div[1]/div/form/input[2]').type('pmidhun1728@gmail.com');
-    cy.xpath('//*[@id="form"]/div/div/div[1]/div/form/input[3]').click().type('Midhun@1994');
-    cy.xpath('//*[@id="form"]/div/div/div[1]/div/form/button').click();
+    cy.get('a[href="/login"]').click(); 
+    cy.get('input[data-qa="login-email"]').type('pmidhun1728@gmail.com');
+    cy.get('input[placeholder="Password"]').click().type('Midhun@1994');
+    cy.get('button[data-qa="login-button"]').click();
   });
 
-  it('Launch URL and Login', () => {
-    cy.get('#shop-menu.pull-right').should('have.text', 'Products');
+  it('Verify Headers list', () => {
+    cy.get('.shop-menu.pull-right').find('ul').each(($ul) => {
+    const expectedNames = ['Home', 'Products', 'Carts', 'Logout', 'Delete Account', 'Test Cases', 'API Testing', ' Video Tutorials', 'Contact us'];   
 
+  }); 
+  
+});
+
+it('Adding Dresses to cart', () => {
+  const expectedNames = ['Dress', 'Tops', 'Saree'];
+
+  cy.xpath("//a[normalize-space()='Women']").click();
+
+  cy.get("div[id='Women'] div[class='panel-body']").find('ul').each(($ul, index) => {
+    const expectedName = expectedNames[index];
+
+    cy.wrap($ul).should('contain', expectedName);
+    cy.log(expectedName);
   });
+});
+
 });
 
